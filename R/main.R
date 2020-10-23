ededfb2848b53a315bb27107c807c69689753c24
Martin Morgan <Martin.Morgan@RoswellPark.org>
2020-10-23 06:22:02 -0400

    use baseenv() in local()

--- R/main.R
+++ R/main.R
@@ -4,8 +4,5 @@
 main <- function(children){
   cl <- makePSOCKcluster(children)
   on.exit(stopCluster(cl), add = TRUE)
-  f <- funs$example
-  clusterExport(cl,"f", env = environment())
-  clusterApply(cl,x = seq.int(children), "f")
-
+  clusterApply(cl,x = seq.int(children), funs$example)
 }
